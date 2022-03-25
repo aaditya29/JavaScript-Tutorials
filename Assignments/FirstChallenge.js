84d7b41c70472753facc9f6a64cd208be58a29ad
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-25 21:08:26 +0530

    AssignmentsAdded

---
+++ Assignments/FirstChallenge.js
@@ -0,0 +1,5 @@
+// 1. Create a variable, myAge, and set its value to your age
+let myAge =22
+
+// 2. Log the myAge variable to the console
+console.log(myAge)
---
+++ Assignments/SecondChallenge.js
@@ -0,0 +1,7 @@
+// 1. Create two variables, myAge and humanDogRatio, 1 Human year = 7 dog years
+let myAge = 22
+let humanDogRatio = 7
+// 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console
+console.log(myDogAge)
