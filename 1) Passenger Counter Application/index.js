6ec094e947f250c9ced07399530052dfa5382316
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-26 00:23:06 +0530

    increment added

--- 1) Passenger Counter Application/index.js
+++ 1) Passenger Counter Application/index.js
@@ -1,9 +1,12 @@
-// document.getElementById("count").innerText = 5
+// document.getElementById("count-el").innerText = 5
 
 let count=0
 
+let countEl = document.getElementById("count-el")
+
 function increment(){
     count = count + 1
+    countEl.innerText = count
     console.log(count)
 }
 
