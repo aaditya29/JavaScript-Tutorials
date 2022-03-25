b4e7d4896bc5c15e1edcb6e12fb4226098cfcbd7
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-25 18:46:43 +0530

    git commit

--- 1) Passenger Counter Application/index.js
+++ 1) Passenger Counter Application/index.js
@@ -1,5 +1,9 @@
 // document.getElementById("count").innerText = 5
 
-let count = 0//variables are declared by the keyword let
+//let count = 0//variables are declared by the keyword let
 
-console.log(count)//console.log is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user
+//console.log(count)//console.log is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user
+
+function increment(){
+    console.log("The button was clicked.")
+}
