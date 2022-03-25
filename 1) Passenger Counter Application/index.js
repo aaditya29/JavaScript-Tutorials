363d17465994c2e531e04a86d011231c5f4ed5dd
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-25 12:38:17 +0530

    git commit

--- 1) Passenger Counter Application/index.js
+++ 1) Passenger Counter Application/index.js
@@ -2,4 +2,4 @@
 
 let count = 0//variables are declared by the keyword let
 
-console.log(count)
+console.log(count)//console.log is used to print any kind of variables defined before in it or to just print any message that needs to be displayed to the user
