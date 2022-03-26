286bf3f84fa7e1fc6131ac0a6df5c7c0c955b731
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-26 11:01:32 +0530

    git commit

--- 1) Passenger Counter Application/index.js
+++ 1) Passenger Counter Application/index.js
@@ -3,6 +3,9 @@
 let count=0
 
 let countEl = document.getElementById("count-el")
+/*The Document method getElementById() returns an Element object representing the element whose id property matches the specified string. Since element IDs are required to be unique if specified, they're a useful way to get access to a specific element quickly.
+Here the 'count-el' matches the specified string in the index.html.
+*/
 
 function increment(){
     count = count + 1
