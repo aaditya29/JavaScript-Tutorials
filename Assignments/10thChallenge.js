bee7089e42d2307f83d3659c9cfe697fab26e6b9
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-28 14:01:41 +0530

    git commit assignment added

---
+++ Assignments/10thChallenge.js
@@ -0,0 +1,8 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el")
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+let name = "aaditya"
+let greeting = "Good to see you back, "
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + name//The innerText property sets or returns the text content of an element.
