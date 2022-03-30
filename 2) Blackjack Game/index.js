cd30756d9e4f67563ff8e53b4126b1aa5fae5f27
Aditya Mishra <mishraaaditya29@gmail.com>
2022-03-30 13:03:20 +0530

    Added start game button

--- 2) Blackjack Game/index.js
+++ 2) Blackjack Game/index.js
@@ -8,7 +8,8 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if(sum<=20){
+function startGame() {
+    if(sum<=20){
     message = "Do you want to draw a new card?"
 }
 else if(sum===21){
@@ -19,7 +20,5 @@ else{
     message= "Sorry. You're out of the game."
     isAlive = false
 }
-
-console.log(hasBlackJack)
-console.log(isAlive)
 console.log(message)
+}
