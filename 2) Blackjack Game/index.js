74352a562d4df2ad81e3cc1f63469a475609825e
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-01 10:51:28 +0530

    displaying the sum

--- 2) Blackjack Game/index.js
+++ 2) Blackjack Game/index.js
@@ -1,16 +1,15 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
 let firstCard = 10
-let secondCard = 11
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let sumEl = document.getElementById("sum-el")
+
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
     if(sum<=20){
     message = "Do you want to draw a new card?"
 }
