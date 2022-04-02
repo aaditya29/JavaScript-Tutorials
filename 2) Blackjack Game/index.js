4f9f4201ab276fcfbca0bfa5ee527731b1900654
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-02 17:09:28 +0530

    random card function added

--- 2) Blackjack Game/index.js
+++ 2) Blackjack Game/index.js
@@ -9,6 +9,10 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    
+}
+
 function startGame() {
     renderGame()
 }
