5bba868b5e5fdf384acd82941d196c3a7507f785
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-02 13:31:26 +0530

    array challenges added

---
+++ Assignments/Second Project Challenges/4thChallenge.js
@@ -0,0 +1,19 @@
+// Arrays - ordered lists of items
+
+let featuredPosts = [
+    "Check out my Netflix clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
+console.log(featuredPosts[1])
+
+let experience = ["CEO at Work", "Frontend developer at I dO Nothing Papa ki pari", "People counter for works at i am studying"]
+
+// Make the following appear in the console:
+// First index
+// Second Index
+// Third Index
+console.log(experience[0])
+console.log(experience[1])
+console.log(experience[2])
+
