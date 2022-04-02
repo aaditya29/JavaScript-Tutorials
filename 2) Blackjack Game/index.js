b9867a24bf96c924245529f049679cca7a223611
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-02 13:02:06 +0530

    added card arrays

--- 2) Blackjack Game/index.js
+++ 2) Blackjack Game/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]//array
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,7 +10,11 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -27,5 +32,5 @@ function newCard(){
     console.log("Drawing a new card from the deck: ")
     let card = 7
     sum += card
-    startGame()
+    renderGame()
 }
