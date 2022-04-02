56ceb5256d344e7c686e66c96c711e5b562c5cc0
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-02 16:05:15 +0530

    array challenges completed

--- Assignments/Second Project Challenges/6thChallenge.js
+++ Assignments/Second Project Challenges/6thChallenge.js
@@ -1,1 +1,18 @@
 let cards = [7,4]
+cards.push(6)//method to push the element in the array
+//console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+messages.push(newMessage)
+console.log(messages)
+//remove the last element from the messages array
+messages.pop()
+console.log(messages)
