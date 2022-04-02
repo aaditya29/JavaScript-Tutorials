832cb7c68a4e2d635c49a1edc838051dac503a4f
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-02 12:49:43 +0530

    added new card functions

--- 2) Blackjack Game/index.js
+++ 2) Blackjack Game/index.js
@@ -8,24 +8,24 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard + " " 
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
-    if(sum<=20){
-    message = "Do you want to draw a new card?"
-}
-else if(sum===21){
-    message = "Congratulations! You got a blackjack."
-    hasBlackJack = true
-}
-else{
-    message= "Sorry. You're out of the game."
-    isAlive = false
-}
-messageEl.textContent = message
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
 function newCard(){
     console.log("Drawing a new card from the deck: ")
+    let card = 7
+    sum += card
+    startGame()
 }
