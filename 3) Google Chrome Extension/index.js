2dceff0b260c33d1e9ccfb8b86f381035ab3336a
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-03 18:42:48 +0530

    Added Text Inputs

--- 3) Google Chrome Extension/index.js
+++ 3) Google Chrome Extension/index.js
@@ -1,5 +1,6 @@
-let inputBtn = document.getElementById("input-btn")
-
+let myLeads = []
+const inputEl = document.createElement('input-el')
+const inputBtn = document.getElementById("input-btn")
 /*
 The addEventListener() method is used to attach an event handler to a particular element. It does not override the existing event handlers. Events are said to be an essential part of the JavaScript. A web page responds according to the event that occurred.
 
