87cf3b45429e883542df48b16f0a09ee4edb3329
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-04 11:54:48 +0530

    Challenges Added

--- Assignments/Third Project Challenges/ThirdChallenge.js
+++ Assignments/Third Project Challenges/ThirdChallenge.js
@@ -5,5 +5,5 @@ container.innerHTML = "<button onclick='buy()'>Buy!</button>"
 // When clicked, render a paragraph under the button (in the container)
 // that says "Thank you for buying!"
 function buy() {
-    container.innerHTML += "<p> Thank You for buying!</p>"
+    container.innerHTML += "<p> Thank You for buying the product!</p>"
 }
