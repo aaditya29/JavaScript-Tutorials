8814b68d18bf5ed972ec02d277576cb6c2009b63
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-04 13:01:46 +0530

    git commit

--- 3) Google Chrome Extension/index.js
+++ 3) Google Chrome Extension/index.js
@@ -24,7 +24,13 @@ function renderLeads(){
     let listItems = " "
     //Logging out the lead values
     for(let i=0; i<myLeads.length; i++){
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"//innerHTML property sets or returns the HTML content of an element.
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
