4d90cef854b15c5696b77ee834ac3e6f64015b33
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-04 12:30:24 +0530

    Render Function

--- 3) Google Chrome Extension/index.js
+++ 3) Google Chrome Extension/index.js
@@ -1,7 +1,6 @@
-let myLeads = ["www.lead.com", "www.awesomelead.com", "www.leadcontent.com"]
-const inputEl = document.createElement('input-el')
+let myLeads = []
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
 const ulEl = document.getElementById("ul-el")
 /*
 The addEventListener() method is used to attach an event handler to a particular element. It does not override the existing event handlers. Events are said to be an essential part of the JavaScript. A web page responds according to the event that occurred.
@@ -13,12 +12,19 @@ Although it has three parameters, the parameters event and function are widely u
 event: It is a required parameter. It can be defined as a string that specifies the event's name.
 function: It is also a required parameter. It is a JavaScript function which responds to the event occur.
 */
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)//Pushing the value from myLeads array to inputEl
-    console.log(myLeads)
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    
+    renderLeads()
 })
 
-//Loggint out the lead values
-for(let i=0; i<myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"//innerHTML property sets or returns the HTML content of an element.
+
+function renderLeads(){
+    //Creating a variable, listItems, to hold all the HTML for the list items and assign it to an empty string to begin with
+    let listItems = " "
+    //Logging out the lead values
+    for(let i=0; i<myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"//innerHTML property sets or returns the HTML content of an element.
+    }
+    ulEl.innerHTML = listItems
 }
