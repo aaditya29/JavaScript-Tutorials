66803615ea108c29446c53c4947ba4e5e1ecea18
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-05 13:33:31 +0530

    Added Functions As Argument Challenges

---
+++ Assignments/Third Project Challenges/12thChallenge.js
@@ -0,0 +1,16 @@
+
+//.                parameters
+function greetUser(greeting, name) { 
+    welcomeEl.textContent = `${greeting}, ${name} 👋`
+}
+
+//.        arguments
+let hi = "Howdy"
+greetUser(hi, "James")
+
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+add(3, 4)
