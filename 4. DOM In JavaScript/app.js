93eb21dfd853ad3785616603b51d452a255cc123
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-15 18:55:48 +0530

    app.js added

---
+++ 4. DOM In JavaScript/app.js
@@ -0,0 +1,9 @@
+/**
+ * The getElementById() method is one of the most common methods in the HTML DOM.
+ * The getElementById() method returns an element with a specified value. The getElementById() method returns null if the element does not exist. 
+ * t is used almost every time you want to read or edit an HTML element.  
+*/  
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
+
+console.log(search, bookList);
