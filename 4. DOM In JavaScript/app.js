5521e8a538ff95ddb7e0a10b0f65a1b763911ca3
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-15 19:16:27 +0530

    getElementById() definition added

--- 4. DOM In JavaScript/app.js
+++ 4. DOM In JavaScript/app.js
@@ -1,7 +1,7 @@
 /**
  * The getElementById() method is one of the most common methods in the HTML DOM.
  * The getElementById() method returns an element with a specified value. The getElementById() method returns null if the element does not exist. 
- * t is used almost every time you want to read or edit an HTML element.  
+ * It is used almost every time you want to read or edit an HTML element.  
 */  
 const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
