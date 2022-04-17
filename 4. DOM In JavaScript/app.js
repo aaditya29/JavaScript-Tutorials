cdc41689e3e210aff3403239db8698c6b9b5a159
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-17 10:54:14 +0530

    git commit

--- 4. DOM In JavaScript/app.js
+++ 4. DOM In JavaScript/app.js
@@ -7,3 +7,12 @@ const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
 console.log(search, bookList)
+
+//Getting Elements By Class or Tag
+
+/**
+ * The getElementsByClassName method of Document interface returns an array-like object of all child elements which have all of the given class name(s).
+ * When called on the document object, the complete document is searched, including the root node. 
+ * This is a live HTMLCollection. Changes in the DOM will reflect in the array as the changes occur. If an element selected by this array no longer qualifies for the selector, it will automatically be removed.
+ */
+const titles = document.getElementsByClassName('title')
