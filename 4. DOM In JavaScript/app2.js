7642bd475ef2eb17702743c55ba0fa0eb1ab90db
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-20 17:58:13 +0530

    Events added in app2.js

---
+++ 4. DOM In JavaScript/app2.js
@@ -0,0 +1,8 @@
+// DOM events
+
+/**
+ * Events are the things that happens inside the browser itself.
+ * Forexample: click,submit, load, input etc.
+ * We add eventListeners to the different elements for which we want our events to react or listen.
+ * For example: If we want to listen the title of a webpage we will attach an event to it to listen to those events. We will also make a callback function that will be called when the event occurs.
+ */
