d1d8e7c757a242145c7e22810326fa459b888be2
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-22 16:34:12 +0530

    Created object

---
+++ 5. OOP In JavaScript/index.js
@@ -0,0 +1,18 @@
+// Creating Object Literals
+
+const circle = {
+    // Object in JS are key value pairs
+    radius: 1,
+    location: {
+        x:1,
+        y:1
+    }, 
+    draw: function(){
+        console.log('draw');
+        /**
+         * The circle object has three members: radius, location and draw function.
+         * Here we are reffering draw as a method.
+         */
+    }
+};// curly brackets refers to objects literals syntax
+circle.draw();// calling the draw method
