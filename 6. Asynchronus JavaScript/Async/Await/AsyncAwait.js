f3c9afce511170fb5276aa65da3aadd2cbe0abf3
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-25 13:02:44 +0530

    git commit

--- 6. Asynchronus JavaScript/Async/Await/AsyncAwait.js
+++ 6. Asynchronus JavaScript/Async/Await/AsyncAwait.js
@@ -1,3 +1,11 @@
+
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"],
+    liquid : ["water", "ice"],
+    holder : ["cone", "cup", "stick"],
+    toppings : ["chocolate", "peanuts"],
+ };
+ 
 let is_shop_open = true;
 
 async function kitchen(){
