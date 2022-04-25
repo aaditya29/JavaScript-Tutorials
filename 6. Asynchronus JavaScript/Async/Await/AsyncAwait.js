89b220d75c9fa0a0c0cf5d462f5c81bf69b566e9
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-25 13:00:56 +0530

    Async Await Completed

---
+++ 6. Asynchronus JavaScript/Async/Await/AsyncAwait.js
@@ -0,0 +1,34 @@
+let is_shop_open = true;
+
+async function kitchen(){
+    //We use the try keyword to run our code while we use catch to catch our errors.
+    try{
+	await time(2000)
+	console.log(`${stocks.Fruits[0]} was selected`)
+
+	await time(0000)
+	console.log("production has started")
+
+	await time(2000)
+	console.log("fruit has been chopped")
+
+	await time(1000)
+	console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+
+	await time(1000)
+	console.log("start the machine")
+
+	await time(2000)
+	console.log(`ice cream placed on ${stocks.holder[1]}`)
+
+	await time(3000)
+	console.log(`${stocks.toppings[0]} as toppings`)
+
+	await time(2000)
+	console.log("Serve Ice Cream")
+    }
+
+    catch(error){
+	 console.log("customer left")
+    }
+}
